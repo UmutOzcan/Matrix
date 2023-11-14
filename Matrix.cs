a76e0fb3da4a15bcf558f274fdc5f5a9a33b884a
Umut Ozcan <umut.yozcan@gmail.com>
2023-11-14 11:23:29 +0300

    methods added

--- Matrix.cs
+++ Matrix.cs
@@ -35,14 +35,181 @@ namespace Matrisler
         /// Parametre olarak aldigi matrisi yazar
         /// </summary>
         /// <param name="matris">Matris</param>
-        public static void Yazdir(int[,] matris) 
+        public static void Yazdir(int[,] matrix) 
         {
-            for (int i = 0; i < matris.GetLength(0); i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for ( int j = 0;j < matris.GetLength(1); j++)
-                    Console.Write("{0,3}",matris[i, j]);
+                for ( int j = 0;j < matrix.GetLength(1); j++)
+                    Console.Write("{0,3}", matrix[i, j]);
                 Console.WriteLine();
             }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// parametre olarak aldigi tek boyutlu matrixi yazar
+        /// </summary>
+        /// <param name="matrix">Matrix</param>
+        public static void Yazdir(int[] matrix )
+        {
+            for (int i = 0; i < matrix.Length; i++)
+                Console.Write("{0,3}", matrix[i]);
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Cizgi cizer
+        /// </summary>
+        public static void Cizgi()
+        {
+            Console.WriteLine("{0}", new String('-', Console.BufferWidth));
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Sifir Matrix olusturur
+        /// </summary>
+        /// <param name="satir">satir sayisi</param>
+        /// <param name="sutun">sutun sayisi</param>
+        /// <returns>Matrix</returns>
+        public static int[,] SifirMatrixOlustur(int satir,int sutun) 
+        {
+            return Olustur(satir, sutun, 0,0);
+        }
+
+        /// <summary>
+        /// Birler Matrix olusturur
+        /// </summary>
+        /// <param name="satir">satir sayisi</param>
+        /// <param name="sutun">sutun sayisi</param>
+        /// <returns>Matrix</returns>
+        public static int[,] BirlerMatrixOlustur(int satir,int sutun) 
+        {
+            return Olustur(satir, sutun, 1, 1);
+        }
+
+        /// <summary>
+        /// Diagonal Matrix olusturur
+        /// </summary>
+        /// <param name="boyut">satir ve sutun sayisi</param>
+        /// <param name="min">min</param>
+        /// <param name="max">max</param>
+        /// <returns>Diagonal Matrix</returns>
+        public static int[,] DiagonalMatrixOlustur(int boyut,int min=1,int max=9)
+        {
+            // Sadece kare matrixler diagonal olabilir
+            // Kosegenler disindaki yerler 0dir 
+            int[,] matrix = SifirMatrixOlustur(boyut, boyut);
+            for (int i = 0; i < boyut; i++)
+                matrix[i,i] = new Random().Next(min,max);
+            return matrix;
+        }
+
+        /// <summary>
+        /// Skaler Matrix Olusturur
+        /// </summary>
+        /// <param name="boyut">satir ve sutun sayisi</param>
+        /// <param name="skaler">kosegen degeri</param>
+        /// <returns>Skaler Matrix</returns>
+        public static int[,] SkalerMatrixOlustur(int boyut=3, int skaler=3)
+        {
+            // Skaler Matrixin kosegen degerleri aynıdır
+            return DiagonalMatrixOlustur(boyut, skaler, skaler);
+        }
+
+        /// <summary>
+        /// satir = sutunsa Kare Matrixtir
+        /// </summary>
+        /// <param name="matrix">matrix</param>
+        /// <returns>bool</returns>
+        public static bool KareMatrixMi(int[,] matrix)
+        {
+            // satir == sutun -> Kare Matrix
+            return matrix.GetLength(0) == matrix.GetLength(1) ? true : false;
+        }
+
+        /// <summary>
+        /// Birim Matrix olusturur
+        /// </summary>
+        /// <param name="boyut">satir sutun sayisi</param>
+        /// <returns>Birim Matrix</returns>
+        public static int[,] BirimMatrixOlustur(int boyut=3)
+        {
+            return SkalerMatrixOlustur(boyut, 1);
+        }
+
+        /// <summary>
+        /// matrix birim mi kontrol eder
+        /// </summary>
+        /// <param name="matrix">matrix</param>
+        /// <returns>bool</returns>
+        public static bool BirimMatrixMi(int[,] matrix)
+        {
+            bool control = false;
+            if(KareMatrixMi(matrix))
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for(int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        if (matrix[i, j] != 0 && i != j)
+                        { control = false; break; }
+                        else
+                        {
+                            if(matrix[i,i] == 1)
+                                control = true;
+                            
+                        }
+                    }
+                } 
+                return control;
+            }
+            else return control;
+        }
+
+        /// <summary>
+        /// Diagonal elemanların listesini dondurur
+        /// </summary>
+        /// <param name="matrix">Matrix</param>
+        /// <returns>Array</returns>
+        public static int[] DiagonalElementsList(int[,] matrix)
+        {
+            if(KareMatrixMi(matrix))
+            {
+                int[] list = new int[matrix.GetLength(0)];
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                    list[i] = matrix[i, i];
+
+                return list;
+            }
+            else
+            {
+                Console.WriteLine("Kare Matrix veriniz !!");
+                return new int[0];
+            }
+        }
+
+        /// <summary>
+        /// Matrix Izini bulur
+        /// </summary>
+        /// <param name="matrix">Matrix</param>
+        /// <returns>int</returns>
+        public static int MatrixIzi(int[,] matrix)
+        {
+            // Matrix Izi kosegen toplamidir
+            int toplam = 0;
+
+            if(KareMatrixMi(matrix))
+            {
+                int[] D = DiagonalElementsList(matrix);
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                    toplam += matrix[i, i];
+                return toplam;
+            }
+            return toplam;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,69 @@
 ﻿using Matrisler;
 
+// constructor degerlerinde Matrix olusturur
 int[,] Y = Matrix.Olustur();
+Console.WriteLine("\n  Constructor Matrixi  \n");
 Matrix.Yazdir(Y);
+Matrix.Cizgi();
+
+// 4-4 Sifir Matrix Olusturur
+int[,] sifirMatrix = Matrix.SifirMatrixOlustur(4, 4);
+Console.WriteLine("  Sifir Matrix  \n");
+Matrix.Yazdir(sifirMatrix);
+Matrix.Cizgi();
+
+// 3-3 Birler Matrix Olusturur
+int[,] birlerMatrix = Matrix.BirlerMatrixOlustur(3, 2);
+Console.WriteLine("  Birler Matrixi  \n");
+Matrix.Yazdir(birlerMatrix);
+Matrix.Cizgi();
+
+// 5-5 Diagonal Matrix Olusturur
+int[,] diagonalMatrix = Matrix.DiagonalMatrixOlustur(5);
+Console.WriteLine("  Diagonal Matrix  \n");
+Matrix.Yazdir(diagonalMatrix);
+Matrix.Cizgi();
+
+// 3-3 Kosegen degeri 6 olan Skaler Matrix Olusturur
+int[,] skalerMatrix = Matrix.SkalerMatrixOlustur(3,6);
+Console.WriteLine("  Skaler Matrix  \n");
+Matrix.Yazdir(skalerMatrix);
+Matrix.Cizgi();
+
+// verilen matrix kare matrix mi
+Console.WriteLine("  Verilen matrix kare matrix mi  \n");
+Matrix.Yazdir(birlerMatrix);
+Console.WriteLine("{0}",Matrix.KareMatrixMi(birlerMatrix) ? "  Kare matrix.\n" : "  Kare matrix degil.\n");
+Matrix.Cizgi();
+
+// default degerleri ile Birim Matrix Olusturur
+Console.WriteLine("  Birim Matrix \n");
+int[,] birimMatrix = Matrix.BirimMatrixOlustur();
+Matrix.Yazdir(birimMatrix);
+Matrix.Cizgi();
+
+// verilen matrix birim matrix mi
+Console.WriteLine("  Verilen matrix birim matrix mi  \n");
+int[,] birimMatrix2 = { { 1, 0, 0 },
+                        { 0, 1, 0 },
+                        { 1, 0, 1 },};
+Matrix.Yazdir(birimMatrix2);
+Console.WriteLine("{0}", Matrix.BirimMatrixMi(birimMatrix2) ? "  Birim matrix.\n" : "  Birim matrix degil.\n");
+Matrix.Cizgi();
+
+
+// Diagonal elamanlar
+Console.WriteLine("  Diagonal Matrix  \n");
+int[,] diagonalMatrix2 = Matrix.DiagonalMatrixOlustur(6);
+Matrix.Yazdir(diagonalMatrix2);
+Console.WriteLine("  Diagonal elemanlar  \n");
+int[] D = Matrix.DiagonalElementsList(diagonalMatrix2);
+Matrix.Yazdir(D);
+
+// Matrix Izi
+Console.WriteLine("  Matrixin Izi = {0} \n",Matrix.MatrixIzi(diagonalMatrix2));
+Matrix.Cizgi();
+
+
+
 Console.ReadKey();
